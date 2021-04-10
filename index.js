18ab7e0662e44739d668cdeefb42d7969b0fc491
vit-401 <vetastektonik1@gmail.com>
2021-04-10 18:29:03 +0300

    added locall api

---
+++ index.js
@@ -0,0 +1,26 @@
+let http = require("http");
+let server = http.createServer((req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Request-Method", "*");
+	res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+	res.setHeader("Access-Control-Allow-Headers", "*");
+	if (req.method === "OPTIONS") {
+		res.writeHead(200);
+		res.end();
+		return;
+	}
+	switch (req.url) {
+		case "/":
+			debugger;
+			res.write("Home Page");
+			break;
+		case "/users":
+			res.write(`[{ "id": 1, "name": "Vitaliy"}, {"id": 2, "name": "Alex" }]`);
+			break;
+		default:
+			res.write("404");
+			break;
+	}
+	res.end();
+});
+server.listen(4000);
